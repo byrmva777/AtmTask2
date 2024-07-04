6e107e8a51dc5681ee37e340e03ecba6c022ca31
user <user@WINDOWS-OQKHAJ5>
2024-07-04 10:31:31 +0400

    Add project files.

---
+++ ConsoleApp10/Atm.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApp10
+{
+    public class Atm
+    {
+        private decimal balance;
+        private string pin;
+        private int pinAttempts;
+
+        public Atm(decimal initialBalance, string initialPIN)
+        {
+            balance = initialBalance;
+            pin = initialPIN;
+            pinAttempts = 0;
+        }
+
+        public void EnteredPin(string enteredPIN)
+        {
+            if (pinAttempts >= 3)
+            {
+                throw new Exception("Hesabiniz bloklandi !!!");
+            }
+
+            if (enteredPIN == pin)
+            {
+                pinAttempts = 0;
+                Console.WriteLine("Hesaba daxil olundu");
+            }
+            else
+            {
+                pinAttempts++;
+                throw new Exception("Yalnis pin daxil etdiniz !!!");
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Balansınız:{balance} AZN");
+        }
+
+        public void cashOut(decimal amount)
+        {
+            if (amount > balance)
+            {
+                throw new Exception("Kifayet qeder mebleg movcud deyil");
+            }
+            balance -= amount;
+            Console.WriteLine($"Balance: {balance} AZN");
+        }
+
+        public void cashIn(decimal amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Balance: {balance} AZN");
+        }
+        public void ChangePin(string oldPIN, string newPIN)
+        {
+            if (oldPIN != pin)
+            {
+                throw new Exception("Pini duzgun daxil edin");
+            }
+            pin = newPIN;
+            Console.WriteLine("Pin deyisdirildi");
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,83 @@
+﻿using ConsoleApp10;
+
+namespace AtmSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atm atm = new Atm(1000, "1234");
+
+            Console.WriteLine("PIN kodunuzu daxil edin:");
+            string pin = Console.ReadLine();
+            try
+            {
+                atm.EnteredPin(pin);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("-----ATM-----:");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Cash Out");
+                Console.WriteLine("3. Cash Inn");
+                Console.WriteLine("4. Change Pin");
+                Console.WriteLine("5. Exit");
+                Console.Write("Seçiminizi daxil edin: ");
+                string choice = Console.ReadLine();
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case "1":
+                            atm.CheckBalance();
+                            break;
+                        case "2":
+                            Console.Write("Meblegi daxil edin: ");
+                            decimal amountOut = Convert.ToDecimal(Console.ReadLine());
+                            atm.cashOut(amountOut);
+                            break;
+                        case "3":
+                            Console.Write("Meblegi daxil edin: ");
+                            decimal amountIn = Convert.ToDecimal(Console.ReadLine());
+                            atm.cashIn(amountIn);
+                            break;
+                        case "4":
+                            Console.Write("Kohne pini daxil edin: ");
+                            string oldPin = Console.ReadLine();
+                            Console.Write("Yeni pini daxil edin: ");
+                            string newPin = Console.ReadLine();
+                            atm.ChangePin(oldPin, newPin);
+                            break;
+                        case "5":
+                            Console.WriteLine("Exit");
+                            return;
+                        default:
+                            Console.WriteLine("Yanlıs secim etdiniz!!!");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
